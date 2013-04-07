0e0c84bec905a2c412b7b64999a9a2fdc0459278
李朋臻 <b99902008@ntu.csie.edu.tw>
2013-04-08 07:19:38 +0800

    Working on Demo, cancel function call

--- src/Demo.java
+++ src/Demo.java
@@ -1,7 +1,6 @@
 import POO_BBS.*;
 import java.util.Scanner;
 public class Demo {
-	private static String[] cmds = {"r","a"};
 	
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
@@ -9,16 +8,77 @@ public class Demo {
 		User user = new User();
 		Entry[] history = new Entry[1024];
 		history[0] = home;
-		Integer current = 0;
-		Boolean exit = false;
+		int current = 0;
+		boolean exit = false;
 		while(true){
 			System.out.print("Please enter your username: ");
 			user.setUserName(input.nextLine());
+			
 			while(!exit){
 				history[current].show();
-				doCommand(user, history, current, exit, input);
-				System.out.println(exit);
+				
+				/* doCommand */
+				Entry.TYPE type = history[current].getType();
+				boolean correct = false;
+				while(!correct){
+					
+					/* printInfo */
+					switch(type){
+						case ARTICLE:
+							System.out.print("r)Return e)Exit :");
+							break;
+						case BOARD:
+							System.out.print("a)Add a article r)Return e)Exit:");
+							break;
+						case DIRECTORY:
+							System.out.print("a)Add a article r)Return e)Exit:");
+							break;
+						default:
+							System.out.println("Error Type");
+							break;
+					}
+					
+					/* parse input */
+					correct = true;
+					String tmp = input.nextLine();
+					switch(tmp){
+						case "r":
+						case "R":
+						if(current != 0)
+								current--;
+							break;
+						case "e":
+						case "E":
+							exit = true;
+							break;
+						case "a":
+						case "A":
+							if(type != Entry.TYPE.BOARD && type != Entry.TYPE.DIRECTORY)
+								correct = false;
+							String title;
+							String content;
+							System.out.print("Title\t: ");
+							title = input.nextLine();
+							System.out.print("Content\t: ");
+							content = input.nextLine();
+							POOArticle art = new POOArticle(history[current].length(), title, user.getUserName(), content);
+							if(type == Entry.TYPE.BOARD){
+								((POOBoard)history[current]).add(art);
+							}else{
+								((POODirectory)history[current]).add(art);
+							}
+							break;
+						default:
+							System.out.println("Error Command");
+							correct = false;
+					}
+				}
 			}
+			
+			/*while(!exit){
+				history[current].show();
+				doCommand(user, history, current, exit, input);
+			}*/
 		}/*
 		POOBoard board1 = new POOBoard("CF");
 		POOArticle art1 = new POOArticle(1, "Test", "LPJ", "This is a simple test.\nOnly test for Article and Board.\nThe third line.");
@@ -42,67 +102,4 @@ public class Demo {
 		dir1.add(dir2);
 		dir1.show();*/
 	}
-	
-	public static void doCommand(User user, Entry[] history, Integer current, Boolean exit, Scanner input){
-		while(true){
-			printInfo(history[current].getType());
-			if(parseInput(user, history, current, exit, input) == true){
-				break;
-			}
-		}
-	}
-	
-	public static void printInfo(Entry.TYPE type){
-		switch(type){
-			case ARTICLE:
-				System.out.print("r)Return e)Exit :");
-				break;
-			case BOARD:
-				System.out.print("a)Add a article r)Return e)Exit:");
-				break;
-			case DIRECTORY:
-				System.out.print("a)Add a article r)Return e)Exit:");
-				break;
-			default:
-				System.out.println("Error Type");
-				break;
-		}
-	}
-	
-	public static boolean parseInput(User user, Entry[] history, Integer current, Boolean exit, Scanner input){
-		Entry.TYPE type = history[current].getType();
-		String tmp = input.nextLine();
-		switch(tmp){
-			case "r":
-			case "R":
-				if(current != 0)
-					current--;
-				break;
-			case "e":
-			case "E":
-				exit = true;
-				break;
-			case "a":
-			case "A":
-				if(type != Entry.TYPE.BOARD && type != Entry.TYPE.DIRECTORY)
-					return false;
-				String title;
-				String content;
-				System.out.print("Title\t: ");
-				title = input.nextLine();
-				System.out.print("Content\t: ");
-				content = input.nextLine();
-				POOArticle art = new POOArticle(history[current].length(), title, user.getUserName(), content);
-				break;
-			default:
-				System.out.println("Error Command");
-				return false;
-		}
-		return true;
-	}
-}
-
-class data {
-	
-	
 }
